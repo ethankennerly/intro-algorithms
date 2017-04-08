00e73d4bfe8082604f44b3a2b2fb5f6e7d0afbf3
Ethan Kennerly <ethan@lumoslabs.com>
2017-04-08 09:32:49 -0700

    Tested 3 examples of counting sort.

--- countingsort.py
+++ countingsort.py
@@ -1,5 +1,37 @@
 def countingSort(positiveIntegers):
-    pass
+    """Expects each positive integer is between 0 and a finite array size.
+    For simplicity this example is limited to 32-bits.
+    """
+    if not positiveIntegers:
+        return
+    smallest = float('inf')
+    largest = float('-inf')
+    maximum = (1 << 32) - 1
+    minimum = 0
+    for integer in positiveIntegers:
+        if not isinstance(integer, int) or integer < minimum or maximum < integer:
+            raise TypeError('Expected integer between %r and %r. Got %r'
+                % (minimum, maximum, integer))
+        if integer < smallest:
+            smallest = integer
+        if largest < integer:
+            largest = integer
+    length = largest + 1
+    counts = [0] * length
+    for integer in positiveIntegers:
+        counts[integer] += 1
+    for index in range(1, length):
+        counts[index] += counts[index - 1]
+    sortedIndex = len(positiveIntegers)
+    for index in range(largest, -1, -1):
+        if 1 <= index:
+            previousCount = counts[index - 1]
+        else:
+            previousCount = 0
+        while 1 <= sortedIndex and previousCount < counts[index]:
+            sortedIndex -= 1
+            positiveIntegers[sortedIndex] = index
+            counts[index] -= 1
 
 
 if '__main__' == __name__:
--- testCountingsort.py
+++ testCountingsort.py
@@ -4,10 +4,23 @@ from countingsort import countingSort
 
 
 class TestCountingSort(TestCase):
-    def testCountingSort(self):
+    def testCountingSortEmpty(self):
+        integers = []
+        countingSort(integers)
+        self.assertEquals(integers, [])
+
+    def testCountingSortDuplicate(self):
         integers = [0, 3, 2, 0]
         countingSort(integers)
         self.assertEquals(integers, [0, 0, 2, 3])
+
+    def testCountingSortMixedTypeError(self):
+        mixedTypes = [0, 3, '2', 'a']
+        self.assertRaises(TypeError, countingSort, mixedTypes)
+
+    def testCountingSortNegativeError(self):
+        mixedTypes = [0, 2, -1]
+        self.assertRaises(TypeError, countingSort, mixedTypes)
 
 
 if '__main__' == __name__:
