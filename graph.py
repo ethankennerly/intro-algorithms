19578dde03d2f8823478dbede0ce2d5fe4359e6f
Ethan Kennerly <ethan@lumoslabs.com>
2017-04-09 09:39:54 -0700

    Tested four examples of graph has cycle.

--- graph.py
+++ graph.py
@@ -1,2 +1,26 @@
 def hasCycle(adjacencyLists):
+    """
+    Depth-first search.
+    Return if any adjacent node is already in this depth-first tree.
+    """
+    nodes = adjacencyLists.keys()
+    searchPhases = {}
+    for node in nodes:
+        searchPhases[node] = 0
+    fromNodes = []
+    parents = []
+    for source in nodes:
+        if hasDepthFirstCycle(adjacencyLists, source, searchPhases):
+            return True
+    return False
+
+
+def hasDepthFirstCycle(adjacencyLists, fromNode, searchPhases):
+    for toNode in adjacencyLists[fromNode]:
+        if searchPhases[toNode] <= 0:
+            searchPhases[toNode] = 1
+            return hasDepthFirstCycle(adjacencyLists, toNode, searchPhases)
+        elif 1 == searchPhases[toNode]:
+            return True
+    searchPhases[fromNode] = 2
     return False
--- testGraph.py
+++ testGraph.py
@@ -8,13 +8,18 @@ class TestGraph(TestCase):
         adjacencyLists = {0: [1, 2], 1: [], 2: []}
         self.assertEquals(hasCycle(adjacencyLists), False)
 
-    def testHasCycleMerge(self):
-        adjacencyLists = {0: [1, 2], 1: [], 2: [1]}
-        self.assertEquals(hasCycle(adjacencyLists), False)
-
     def testHasCycleLoop(self):
         adjacencyLists = {0: [1, 2], 1: [], 2: [0]}
         self.assertEquals(hasCycle(adjacencyLists), True)
+
+    def testHasCycleMergeLeaf(self):
+        adjacencyLists = {0: [1, 2], 1: [], 2: [1]}
+        self.assertEquals(hasCycle(adjacencyLists), False)
+
+    def testHasCycleMerge(self):
+        adjacencyLists = {0: [1, 2], 1: [3], 2: [1], 3: []}
+        self.assertEquals(hasCycle(adjacencyLists), False)
+
 
 
 if '__main__' == __name__:
