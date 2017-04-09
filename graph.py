8fc53de8ecef76d39214fd395d00233c0f7ee9f1
Ethan Kennerly <ethan@lumoslabs.com>
2017-04-09 11:20:29 -0700

    Tested two examples of topological sort.

--- graph.py
+++ graph.py
@@ -1,14 +1,12 @@
 def hasCycle(adjacencyLists):
     """
-    Depth-first search.
+    Recursive depth-first search as described in the book adapted for detecting a cycle.
     Return if any adjacent node is already in this depth-first tree.
     """
     nodes = adjacencyLists.keys()
     searchPhases = {}
     for node in nodes:
         searchPhases[node] = 0
-    fromNodes = []
-    parents = []
     for source in nodes:
         if hasDepthFirstCycle(adjacencyLists, source, searchPhases):
             return True
@@ -24,3 +22,30 @@ def hasDepthFirstCycle(adjacencyLists, fromNode, searchPhases):
             return True
     searchPhases[fromNode] = 2
     return False
+
+
+def topologicalSort(adjacencyLists):
+    """
+    Return list of nodes sorted by predecessors first.
+    Error if the graph has a cycle.
+    """
+    nodes = adjacencyLists.keys()
+    searchPhases = {}
+    for node in nodes:
+        searchPhases[node] = 0
+    order = []
+    for source in nodes:
+        postOrderSearch(adjacencyLists, source, searchPhases, order)
+    return order
+
+
+def postOrderSearch(adjacencyLists, fromNode, searchPhases, order):
+    for toNode in adjacencyLists[fromNode]:
+        if searchPhases[toNode] <= 0:
+            searchPhases[toNode] = 1
+            postOrderSearch(adjacencyLists, toNode, searchPhases, order)
+        elif 1 == searchPhases[toNode]:
+            raise Exception('Graph contains cycle at %r. Graph %r.' % (toNode, adjacencyLists))
+    if fromNode not in order:
+        order.insert(0, fromNode)
+    searchPhases[fromNode] = 2
--- testGraph.py
+++ testGraph.py
@@ -20,6 +20,13 @@ class TestGraph(TestCase):
         adjacencyLists = {0: [1, 2], 1: [3], 2: [1], 3: []}
         self.assertEquals(hasCycle(adjacencyLists), False)
 
+    def testTopologicalSort(self):
+        adjacencyLists = {0: [1, 2], 1: [3], 2: [1], 3: []}
+        self.assertEquals(topologicalSort(adjacencyLists), [0, 2, 1, 3])
+
+    def testTopologicalSortLoopError(self):
+        adjacencyLists = {0: [1, 2], 1: [], 2: [0]}
+        self.assertRaises(Exception, topologicalSort, adjacencyLists)
 
 
 if '__main__' == __name__:
