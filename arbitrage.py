b0c95b05a355ccf78f3cc4f844e27beb76eb0ae8
Ethan Kennerly <ethan@lumoslabs.com>
2017-04-15 07:23:38 -0700

    Stubbed detect arbitrage function.

---
+++ arbitrage.py
@@ -0,0 +1,12 @@
+def isArbitrage(exchangeRateMatrix):
+    """
+    Abitrage: Profiting from discrepancies in exchange rate.
+
+    isArbitrage function returns if there is a path of exchanges that yields a profit.
+
+    exchangeRateMatrix:  Weighted adjacency matrix.
+    M[i][j] represents exchange rate from currency i to j.
+
+    Answering question 24-3 on page 679.
+    """
+    return False
