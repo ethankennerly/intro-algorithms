1a19c0f693c120d7c64913da1fb18ff47ded71fd
Ethan Kennerly <ethan@lumoslabs.com>
2017-04-20 07:53:28 -0700

    Tested pre/post/level traversal of 1 binary tree.

--- binarytree.py
+++ binarytree.py
@@ -1,3 +1,6 @@
+from collections import deque
+
+
 class Node(object):
     def __init__(self, key):
         self.key = key
@@ -30,4 +33,43 @@ def inOrderTraverse(node):
     keys.append(node.key)
     if node.children[1]:
         keys.extend(inOrderTraverse(node.children[1]))
+    return keys
+
+
+def preOrderTraverse(node):
+    keys = []
+    if node is None:
+        return keys
+    if node.children[0]:
+        keys.extend(preOrderTraverse(node.children[0]))
+    if node.children[1]:
+        keys.extend(preOrderTraverse(node.children[1]))
+    keys.append(node.key)
+    return keys
+
+
+def postOrderTraverse(node):
+    keys = []
+    if node is None:
+        return keys
+    keys.append(node.key)
+    if node.children[0]:
+        keys.extend(postOrderTraverse(node.children[0]))
+    if node.children[1]:
+        keys.extend(postOrderTraverse(node.children[1]))
+    return keys
+
+
+def levelOrderTraverse(root):
+    keys = []
+    if root is None:
+        return keys
+    queue = deque()
+    queue.append(root)
+    while queue:
+        node = queue.popleft()
+        keys.append(node.key)
+        for child in node.children:
+            if child:
+                queue.append(child)
     return keys
--- testbinarytree.py
+++ testbinarytree.py
@@ -3,14 +3,28 @@ from unittest import TestCase, main
 from binarytree import *
 
 
+root = createTree([4,
+                    [[2,
+                       [1, 3]],
+                      6]])
+
+
 class TestBinaryTree(TestCase):
     def testInOrderTraverse(self):
-        root = createTree([4,
-                            [[2,
-                               [1, 3]],
-                              6]])
         self.assertEqual(inOrderTraverse(root),
             [1, 2, 3, 4, 6]);
+
+    def testPreOrderTraverse(self):
+        self.assertEqual(preOrderTraverse(root),
+            [1, 3, 2, 6, 4]);
+
+    def testPostOrderTraverse(self):
+        self.assertEqual(postOrderTraverse(root),
+            [4, 2, 1, 3, 6]);
+
+    def testLevelOrderTraverse(self):
+        self.assertEqual(levelOrderTraverse(root),
+            [4, 2, 6, 1, 3]);
 
 
 if '__main__' == __name__:
