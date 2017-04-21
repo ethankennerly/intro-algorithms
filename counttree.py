696336e0783a75dafc7e1afdd90feacc2eedebf3
Ethan Kennerly <ethan@lumoslabs.com>
2017-04-21 09:09:53 -0700

    Fail to run from syntax error.

--- counttree.py
+++ counttree.py
@@ -1,4 +1,4 @@
-def countSmallerToTheRight(numbers):
+def countSmallerToTheRight(values):
     """
     Codefights.com interview practice question from Google by the same name.
 
@@ -9,7 +9,57 @@ def countSmallerToTheRight(numbers):
     I read nneoneo's algorithm outline to count in the binary tree.
     http://stackoverflow.com/questions/25251340/in-a-bst-find-count-of-nodes-smaller-greater-than-a-given-node-in-olog-n
     """
-    print('countSmallerToTheRight: TODO')
-    length = len(numbers)
+    if not values:
+        return []
+    length = len(values)
     counts = [0] * length
+    indexes = range(length - 2, -1, -1)
+    root = CountTreeNode(values[0])
+    for index in indexes:
+        value = values[index]
+        child = insert(root, value)
+        counts[index] = countSmaller(child)
     return counts
+
+
+class CountTreeNode(object):
+    def __init__(self, value):
+        self.value = value
+        self.parent = None
+        self.numDescendents = 0
+        self.children = [None, None]
+
+
+def insert(root, value):
+    parent = root
+    while True:
+        parent.numDescendents += 1
+        childIndex = 1 if parent.value < value else 0
+        children = parent.children
+        child = children[childIndex]
+        if child:
+            parent = child
+        else:
+            child = CountTreeNode(value)
+            children[childIndex] = child
+            child.parent = parent
+            return child
+
+
+def countSmaller(newChild):
+    count = 0
+    smaller = child.children[0]
+    if smaller:
+        count += smaller.numDescendents
+    child = newChild
+    parent = child.parent
+    while parent:
+        isGreater = parent.children[1] == child:
+        if isGreater:
+            count += 1
+            smaller = parent.children[0]
+            if smaller:
+                count += smaller.numDescendents
+        child = parent
+        parent = child.parent
+    return count
